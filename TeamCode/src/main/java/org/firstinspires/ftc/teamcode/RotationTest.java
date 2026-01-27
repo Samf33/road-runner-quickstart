34359f06ab5da209286a3f1662b6368cc17128b8
AlonSakin <29asakin@gannacademy.org>
2026-01-27 18:24:21 -0500

    Create RotationTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotationTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(group = "drive")
+public class RotationTest extends LinearOpMode {
+
+    MecanumDrive drive;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        drive = new MecanumDrive(hardwareMap, new Pose2d(0,0, 0));
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+            Pose2d pose = drive.localizer.getPose();
+            double targetHeading = Math.atan2(-pose.position.x, -pose.position.y);
+
+            drive.setDrivePowers(new PoseVelocity2d(new Vector2d(0, 0), pose.heading.real - targetHeading));
+
+            drive.updatePoseEstimate();
+
+            telemetry.update();
+        }
+    }
+}
